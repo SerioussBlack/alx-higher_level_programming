a89e9a6007e634c848992265a9b4da22eb5c0d20
SerioussBlack <bernardnewman70@gmail.com>
2023-09-28 00:47:46 +0000

    all task

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,10 @@
-#!/usr/bin/node
-const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-	  console.log(`code: ${response.statusCode}`);
+t request = require('request');
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
+	  if (err) {
+		      console.log(err);
+		    } else {
+			        console.log('code: ' + response.statusCode);
+			      }
 });
